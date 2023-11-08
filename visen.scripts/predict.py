ed3ebe08cce5eb832348f1b867ee95753bf3a3ff
Visien <1511624535@qq.com>
2023-11-08 11:02:40 +0800

    edit

--- visen.scripts/predict.py
+++ visen.scripts/predict.py
@@ -10,17 +10,16 @@ model = YOLO('pretrain_models/3-v8n-100.pt')  # load an official model
 img_path = 'visen.scripts/test_example/'
 # model.predict(source=img_path, save=True, imgsz=640, device=[0,1])
 
-img_ls = [img_path+i for i in os.listdir(img_path)]
+img_ls = [img_path + i for i in os.listdir(img_path)]
 print(f'img_ls: {img_ls}')
-class_dic = {0:'person',1:'head',2:'helmet'}
+class_dic = {0: 'person', 1: 'head', 2: 'helmet'}
 
 results = model(img_ls, stream=True, device='cpu')
-for i,result in zip(range(len(img_ls)), results):
-    boxes = result.boxes  # Boxes object for bbox outputs
-    # print(boxes)
+for i, result in zip(range(len(img_ls)), results):
+    boxes = result.boxes
     cls = [class_dic[i] for i in boxes.cls.tolist()]
     print(img_ls[i])
     print(cls)
     print(boxes.xywh)
-    
-    print('----'*20)
+
+    print('----' * 20)
